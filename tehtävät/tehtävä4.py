9e55df10bdacdfb45e2ec994d23bcf4abd78e41c
Matti Pihlajaviita <matti.pihlajaviita@hotmail.com>
2026-01-13 15:36:37 +0200

    commit

---
+++ tehtävät/tehtävä4.py
@@ -0,0 +1,18 @@
+print("Anna minulle 3 kokonaislukua, niin kerron näiden lukujen summan, tulon ja keski-arvon")
+
+luku1 = float(input("Ensimmäinen luku:"))
+luku2 = float(input("Toinen luku:"))
+luku3 = float(input("Ja kolmas luku:"))
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo= (luku1 + luku2 + luku3) / 3
+print("Lukujen summa on:", summa)
+print("Lukujen tulo on:", tulo)
+print("lukujen keskiarvo on:", keskiarvo)
+
+
+
+
+
+
+
