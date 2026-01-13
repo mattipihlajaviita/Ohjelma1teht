99b664fff2d585c5651bf5c04598ecac91cf8b10
Matti Pihlajaviita <matti.pihlajaviita@hotmail.com>
2026-01-13 15:05:18 +0200

    commit

--- tehtävät/tehtävä3.py
+++ tehtävät/tehtävä3.py
@@ -1,1 +1,6 @@
-print("Mikä on suorakulmion kanta ja korkeus?")
+kanta = float(input("Paljonko on suorakulmion kanta?"))
+korkeus = float(input("Entä mikä on suorakulmion korkeus?"))
+piiri = (kanta * 2) + (korkeus * 2)
+area = kanta * korkeus
+print("Suorakulmion piiri on: ",piiri)
+print("Suorakulmion pinta-ala on:", area)
