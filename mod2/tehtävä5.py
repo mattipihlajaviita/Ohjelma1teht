0b71f18dafa9db2a2cbc2b6e78adf04c76d82bb6
Matti Pihlajaviita <matti.pihlajaviita@hotmail.com>
2026-01-16 14:41:23 +0200

    commit

--- mod2/tehtävä5.py
+++ mod2/tehtävä5.py
@@ -1,16 +1,20 @@
 #1 leiviskä = 20 naulaa
 #1 naula = 32 luotia
 #1 1 luoti = 13.3 grammaa
-leiviska = int(input("Anna leiviskät:"))
-naula = int(input("Anna naulat:"))
-luoti = int(input("Anna luodit:"))
+leiviska = float(input("Anna leiviskät:"))
+naula = float(input("Anna naulat:"))
+luoti = float(input("Anna luodit:"))
 
-luotipaino1 = 13.3
+luotipaino1 = 0.0133
 naulapaino1 = luotipaino1 * 32
 leiviskapaino1 = naulapaino1 * 20
 
 massa = (luoti * luotipaino1) + (naulapaino1 * naula) + (leiviska * leiviskapaino1)
-print("Massa nykymittojen mukaan on:", massa,"grammaa" )
+
+kilot = int(massa)
+grammat = round((massa - kilot) * 1000)
+print("Massa nykymittojen mukaan:")
+print(kilot,"kilogrammaa ja",grammat,"grammaa")
 
 
 
---
+++ mod2/tehtävä6.py
@@ -0,0 +1,11 @@
+import random
+nro1 = random.randint (0,9)
+nro2 = random.randint (0,9)
+nro3 = random.randint (0,9)
+print(nro1,nro2,nro3)
+
+noppa1 = random.randint(1,6)
+noppa2 = random.randint(1,6)
+noppa3 = random.randint(1,6)
+noppa4 = random.randint(1,6)
+print(noppa1,noppa2,noppa3,noppa4)
