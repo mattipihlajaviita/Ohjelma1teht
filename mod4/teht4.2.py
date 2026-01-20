3567613469c46933003979c389e81d64d03d93ee
Matti Pihlajaviita <matti.pihlajaviita@hotmail.com>
2026-01-20 15:55:32 +0200

    a

--- mod4/teht4.2.py
+++ mod4/teht4.2.py
@@ -1,18 +1,10 @@
 #Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes
 #käyttäjä antaa negatiivisen tuumamäärän.
 #Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm
-
-while True:
+tuumat = float(input("Montako tuumaa?"))
+while tuumat >=0:
+    print(tuumat * 2.54, "cm")
     tuumat = float(input("Montako tuumaa?"))
-    print((tuumat * 2.54), "cm")
     if tuumat < 0:
         print("Ohjelma lopetetaan.")
-        break
-
-
-
-
-
-
-
 
