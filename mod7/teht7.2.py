0d58f695afc71582704a7f1e71584c63a5c20f26
Matti Pihlajaviita <matti.pihlajaviita@hotmail.com>
2026-02-03 20:01:55 +0200

    teht 7.2 tehty

--- mod7/teht7.2.py
+++ mod7/teht7.2.py
@@ -4,17 +4,37 @@ jälkeen ohjelma tulostaa joko tekstin Uusi nimi tai Aiemmin syötetty
 nimi sen mukaan, syötettiinkö nimi ensimmäistä kertaa. Lopuksi ohjelma
 luettelee syötetyt nimet yksi kerrallaan allekkain mielivaltaisessa
 järjestyksessä. Käytä joukkotietorakennetta nimien tallentamiseen."""
+#luodaan joukko: nimet
 nimet = set()
+
+#pyydetään käyttäjää syöttämään nimiä
 nimi = input("Syötä nimiä: ")
-nimet.add(nimi)
+
+#luodaan silmukka joka päättyy kun käyttäjä syöttää tyhjän merkkijonon
 while nimi != "":
-    if nimi in nimet:
-        print("Aiemmin syötetty nimi.")
+
+    #jos käyttäjän syöttämä nimi ei ole joukossa, lisätään se
+    if nimi not in nimet:
+        print("Uusi nimi")
+        nimet.add(nimi)
+
+    #jos käyttäjän nimi on joukossa, ilmoitetaan siitä
+    elif nimi in nimet:
+
+        print("Aiemmin syötetty nimi")
+    #pyydetään käyttäjää syöttämään uusi nimi
+    nimi = input("Syötä nimiä: ")
+
+#silmukka päättyy ja printataan joukossa olevat nimet allekkain
 else:
-    print("Uusi nimi syötetty.")
-nimi = input("Syötä nimiä: ")
-for i in nimet:
-    print(nimet)
+    for i in nimet:
+        print(i)
+
+
+
+
+
+
 
 
 
